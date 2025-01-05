2f8f5e314212e745b0822a102dd415b57401dda7
T0ny-dev <antonioraweb@gmail.com>
2025-01-04 22:00:35 -0600

    update 151 pokemons

--- src/aJaxApiPokemon/AjaxApi.js
+++ src/aJaxApiPokemon/AjaxApi.js
@@ -1,59 +1,70 @@
 import React from "react";
-import'./AjaxApi.css'
+import "./AjaxApi.css";
 
-function Pokemon (props){
-  return <figure>
-    <img src={props.avatar} alt={props.name}></img>
-    <figcaption>name : {props.name}</figcaption>
-    <span>type : {props.type}</span>
-    <span className="attack">attack: {props.attack}</span>
-    
-  </figure>
+function Pokemon(props) {
+  return (
+    <figure>
+      <img src={props.avatar} alt={props.name} />
+      <figcaption>Name: {props.name}</figcaption>
+      <span>Type: {props.type}</span>
+      <span className="attack">Attack: {props.attack}</span>
+    </figure>
+  );
 }
 
-class AjaxApiPokemon extends React.Component{
+class AjaxApiPokemon extends React.Component {
   state = {
-    pokemons :[],
+    pokemons: [],
+    loading: true,
+    error: null,
   };
 
-  componentDidMount() {
-    let url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100";
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        json.results.forEach((el) => {
-          fetch(el.url)
-            .then((res) => res.json())
-            .then((json) => {
-              console.log(json);
-              let pokemon = {
-                id:json.id,
-                name:json.name,
-                avatar:json.sprites.front_default,
-                type:json.types[0].type.name,
-                attack:json.stats[1].base_stat
-              };
+  async componentDidMount() {
+    const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151"; // Cambiado a 151
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
 
-              let pokemons=[...this.state.pokemons, pokemon];
-              this.setState({pokemons});
-            });
+      const pokemonDetails = await Promise.all(
+        data.results.map(async (el) => {
+          const res = await fetch(el.url);
+          const pokemon = await res.json();
+          return {
+            id: pokemon.id,
+            name: pokemon.name,
+            avatar: pokemon.sprites.front_default,
+            type: pokemon.types[0]?.type.name || "Unknown", // Manejo de tipos faltantes
+            attack: pokemon.stats[1]?.base_stat || 0, // Manejo de ataques faltantes
+          };
         })
-      })
+      );
+
+      this.setState({ pokemons: pokemonDetails, loading: false });
+    } catch (error) {
+      this.setState({ error: "Failed to fetch Pokémon data.", loading: false });
+    }
   }
 
   render() {
+    const { pokemons, loading, error } = this.state;
+
     return (
       <div className="Container">
         <h2>Minimal Pokedex</h2>
         <div className="Containers__pokemons">
-        {this.state.pokemons.length === 0 ? (
-          <h3>loading...</h3>
-        ) : (
-          this.state.pokemons.map((el) => (
-            <Pokemon key={el.id} name={el.name} avatar={el.avatar} type ={el.type} attack={el.attack}/>
-          ))
-        )}
+          {loading && <h3>Loading...</h3>}
+          {error && <h3>{error}</h3>}
+          {!loading &&
+            !error &&
+            pokemons.map((el) => (
+              <Pokemon
+                key={el.id}
+                name={el.name}
+                avatar={el.avatar}
+                type={el.type}
+                attack={el.attack}
+              />
+            ))}
         </div>
       </div>
     );
